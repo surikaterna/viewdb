afe0e0569c356fbd86f7f607f9573fa71124c06e
Andreas Karlsson <sprawl@onlajn.nu>
2016-08-18 21:30:38 +0200

    late commit, unknown changes ...

--- test/cursor.js
+++ test/cursor.js
@@ -1,28 +1,39 @@
 var Cursor = require('../lib/cursor');
 var should = require('should');
 
-describe('Cursor', function() {
-	it('#toArray', function(done) {
-		var cursor = new Cursor(null, {}, null, function(query, callback) {
-			callback(null, [1,2,3,4]);
-		});
-		cursor.toArray(function(err, result) {
-			result.length.should.equal(4);
-			done();
-		});
-	});
-	it('#forEach', function(done) {
-		var cursor = new Cursor(null, {}, null, function(query, callback) {
-			callback(null, [1,2,3,4]);
-		});
-		var calls = 0;
-		cursor.forEach(function(result) {
-			result.should.be.ok;
-			calls++;
-		});
-		setTimeout(function() {
-			calls.should.equal(4);
-			done();
-		}, 0);
-	});
+describe('Cursor', function () {
+  it('#toArray', function (done) {
+    var cursor = new Cursor(null, {}, null, function (query, callback) {
+      callback(null, [1, 2, 3, 4]);
+    });
+    cursor.toArray(function (err, result) {
+      result.length.should.equal(4);
+      done();
+    });
+  });
+  it('#forEach', function (done) {
+    var cursor = new Cursor(null, {}, null, function (query, callback) {
+      callback(null, [1, 2, 3, 4]);
+    });
+    var calls = 0;
+    cursor.forEach(function (result) {
+      result.should.be.ok;
+      calls++;
+    });
+    setTimeout(function () {
+      calls.should.equal(4);
+      done();
+    }, 0);
+  });
+  describe('#skip', function () {
+    it.only('should skip x number of documents', function () {
+      var cursor = new Cursor(null, {}, null, function (query, callback) {
+        callback(null, [1, 2, 3, 4]);
+      });
+      cursor.skip(2);
+      cursor.toArray(function(res) {
+        res.length.should.equal(2);
+      });
+    });
+  });
 })
