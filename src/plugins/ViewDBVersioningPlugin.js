6aa1cb809fb2276ed2ed6a5d333ba9c78f311f43
kennyek <ekkenny@hotmail.com>
2021-09-20 00:17:30 +0200

    Update ViewDBVersioningPlugin to class based syntax
    Replace var with const/let
    
    Insert new lines around selections for readability
    
    Since no logic needed to be inside of the class, smaller functions were extracted for cleaner code.
    
    Refactored duplicated code for save/insert to own function.

--- src/plugins/ViewDBVersioningPlugin.js
+++ src/plugins/ViewDBVersioningPlugin.js
@@ -1,67 +1,72 @@
-import _ from 'lodash';
+import { isArray, isUndefined } from 'lodash';
 
-var ViewDBVersioningPlugin = function (viewDb) {
-  var oldCollection = viewDb.collection;
-  viewDb.collection = function () {
-    var coll = oldCollection.apply(this, arguments);
-    if (!coll.__plugins_versioning) {
-      coll.__plugins_versioning = true;
-
-      var oldSave = coll.save;
-      coll.save = function (docs, options) {
-        var newdocs = docs;
-        if (!(options && options.skipVersioning)) {
-          if (!_.isArray(docs)) {
-            newdocs = [docs];
-          }
-          for (var i = 0; i < newdocs.length; i++) {
-            var doc = newdocs[i];
-            doc.version = _getVersion(doc.version);
-          }
-        }
-        oldSave.apply(this, arguments);
-      };
-
-      var oldInsert = coll.insert;
-      coll.insert = function (docs, options) {
-        if (!(options && options.skipVersioning)) {
-          if (!_.isArray(docs)) {
-            docs = [docs];
-          }
-          for (var i = 0; i < docs.length; i++) {
-            var doc = docs[i];
-            doc.version = _getVersion(doc.version);
-          }
-        }
-        oldInsert.apply(this, arguments);
-      };
-
-      var oldFindAndModify = coll.findAndModify;
-      coll.findAndModify = function (query, sort, update, options, cb) {
-        if (!(options && options.skipVersioning)) {
-          var inc = update.$inc || {};
-          inc.version = 1;
-          update.$inc = inc;
-          if (update.$set && update.$set.version >= 0) {
-            delete update.$set.version;
-          }
-        }
-        oldFindAndModify.apply(this, arguments);
-      };
-    }
-    return coll;
-  };
-};
-
-function _getVersion(version) {
-  var newVersion;
-  if (_.isUndefined(version)) {
-    newVersion = 0;
-  } else {
-    newVersion = version + 1;
+export default class ViewDBVersioningPlugin {
+  constructor(viewDb) {
+    mutateCollection(viewDb);
   }
-
-  return newVersion;
 }
 
-export default ViewDBVersioningPlugin;
+function mutateCollection(viewDb) {
+  const oldCollection = viewDb.collection;
+  viewDb.collection = function () {
+    const collection = oldCollection.apply(this, arguments);
+    if (!collection.__plugins_versioning) {
+      collection.__plugins_versioning = true;
+
+      mutateFindAndModify(collection);
+      mutateInsert(collection);
+      mutateSave(collection);
+    }
+    return collection;
+  };
+}
+
+function mutateFindAndModify(collection) {
+  const oldFindAndModify = collection.findAndModify;
+  collection.findAndModify = function (query, sort, update, options, cb) {
+    if (!(options && options.skipVersioning)) {
+      const inc = update.$inc || {};
+      inc.version = 1;
+      update.$inc = inc;
+
+      if (update.$set && update.$set.version >= 0) {
+        delete update.$set.version;
+      }
+    }
+
+    oldFindAndModify.apply(collection, arguments);
+  };
+}
+
+function mutateInsert(collection) {
+  const oldInsert = collection.insert;
+  collection.insert = function (docs, options) {
+    setVersions(docs, options);
+    oldInsert.apply(collection, arguments);
+  };
+}
+
+function mutateSave(collection) {
+  const oldSave = collection.save;
+  collection.save = function (docs, options) {
+    setVersions(docs, options);
+    oldSave.apply(this, arguments);
+  };
+}
+
+function setVersions(docs, options) {
+  if (!(options && options.skipVersioning)) {
+    if (!isArray(docs)) {
+      docs = [docs];
+    }
+
+    for (let i = 0; i < docs.length; i++) {
+      const doc = docs[i];
+      doc.version = getNewVersion(doc.version);
+    }
+  }
+}
+
+function getNewVersion(version) {
+  return isUndefined(version) ? 0 : version + 1;
+}
