7fb3724e4e7e14489bc7dc0ecd1609cd9727d321
Kenny Ek <ekkenny@hotmail.com>
2021-09-20 08:01:36 +0200

    Update ViewDB to class based syntax
    Replace bluebird Promise with native Promise.
    
    Add Babel plugin transform-runtime to support async await.

--- src/ViewDB.js
+++ src/ViewDB.js
@@ -1,22 +1,20 @@
-import Promise from 'bluebird';
 import InMemoryStore from './inmemory/Store';
 
-var ViewDB = function (store) {
-  this._store = store || new InMemoryStore();
-};
-
-ViewDB.prototype.open = function () {
-  if (this._store.open) {
-    return this._store.open().then(function () {
-      return this;
-    });
-  } else {
-    return Promise.resolve(this);
+export default class ViewDB {
+  constructor(store) {
+    this._store = store || new InMemoryStore();
   }
-};
 
-ViewDB.prototype.collection = function (collectionName, callback) {
-  return this._store.collection(collectionName, callback);
-};
+  collection = (collectionName, callback) => {
+    return this._store.collection(collectionName, callback);
+  };
 
-export default ViewDB;
+  open = async () => {
+    if (this._store.open) {
+      await this._store.open();
+      return this;
+    } else {
+      return this;
+    }
+  };
+}
