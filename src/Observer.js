a4ea0bce96a7cf15d4bbe0a663638150a5b31540
Kenny Ek <ekkenny@hotmail.com>
2021-09-20 05:12:46 +0200

    Update Observer to class based syntax
    Extract mergeOptions so that it won't have to be recreated on each refresh.

--- src/Observer.js
+++ src/Observer.js
@@ -1,72 +1,53 @@
-import _ from 'lodash';
+import { defaults, get } from 'lodash';
 import merge from './merge';
 
-var logger = {
-  added: function () {
-    console.log('added: ');
-    console.log(arguments);
-  },
-  removed: function () {
-    console.log('removed: ');
-    console.log(arguments);
-  },
-  changed: function () {
-    console.log('changed: ');
-    console.log(arguments);
-  },
-  moved: function () {
-    console.log('moved: ');
-    console.log(arguments);
+export default class Observer {
+  constructor(query, queryOptions, collection, options) {
+    this._query = query;
+    this._options = options;
+    this._collection = collection;
+    this._cache = [];
+
+    this._mergeOptions = defaults(
+      {
+        comparatorId: indexedIdComparator
+      },
+      this._options
+    )
+
+    const listener = () => {
+      this.refresh();
+    };
+
+    collection.on('change', listener);
+    this.refresh(true);
+
+    return {
+      stop: () => {
+        this._cache = null;
+        collection.removeListener('change', listener);
+      }
+    };
   }
-};
 
-var Observer = function (query, queryOptions, collection, options) {
-  this._query = query;
-  this._queryOptions = queryOptions;
+  refresh = (initial) => {
+    this._collection._getDocuments(this._query, (err, result) => {
+      if (initial && this._options.init) {
+        this._cache = result;
+        this._options.init(result);
+      } else {
+        const old = this._cache;
 
-  this._options = options;
-  //this._options = logger;
-  this._collection = collection;
-  this._cache = [];
-  var self = this;
-  var listener = function () {
-    self.refresh();
+        this._cache = merge(
+          old,
+          result,
+          this._mergeOptions
+        );
+      }
+    });
   };
-  collection.on('change', listener);
-  this.refresh(true);
-  return {
-    stop: function () {
-      this._cache = null;
-      collection.removeListener('change', listener);
-    }
-  };
-};
+}
 
-Observer.prototype.refresh = function (initial) {
-  var self = this;
-
-  this._collection._getDocuments(this._query, function (err, result) {
-    if (initial && self._options.init) {
-      self._cache = result;
-      self._options.init(result);
-    } else {
-      var old = self._cache;
-
-      self._cache = merge(
-        old,
-        result,
-        _.defaults(
-          {
-            comparatorId: function (a, b) {
-              return _.get(a, '_id') === _.get(b, '_id');
-            }
-          },
-          self._options
-        )
-      );
-      //rewind cursor for next query...
-    }
-  });
-};
-
-export default Observer;
+function indexedIdComparator (firstDocument, secondDocument) {
+  return get(firstDocument, '_id') === get(secondDocument, '_id')
+}
