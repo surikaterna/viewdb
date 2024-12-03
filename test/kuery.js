c471b23c53eba590d87e628bff6b0dfb1b499237
galbertsson <42936045+galbertsson@users.noreply.github.com>
2024-12-03 11:08:30 +0100

    Fixed $elemMatch not working using dot notation for property in array (#21)
    * Fixed $elemMatch not working using dot notation for property in array
    
    * added package-lock
    
    * bump version
    
    * Added more test cases
    
    * Added additional tests for elemMatch
    
    * added logic for no  on object property
    
    * Added prettier and updated files
    
    * optional nested array elemMatch testcase
    
    * added  to optional array elemmatch to use same query that works in loki
    
    * fix $elemMatch query on optional nesten array props
    
    * add more tests
    
    * bump minor version
    
    * add tests for deep elem match with differing nested types
    
    * return instead of clearing results to make elemMatch logic more correct
    
    ---------
    
    Co-authored-by: Gustav Albertsson <gustav.albertsson@surikat.se>
    Co-authored-by: spralle <sprawl@onlajn.nu>
    Co-authored-by: Emil Forsmark <emil.forsmark@surikat.se>
    Co-authored-by: cazhjo <54894732+cazhjo@users.noreply.github.com>

--- test/kuery.js
+++ test/kuery.js
@@ -1,19 +1,134 @@
 var _ = require('lodash/fp');
+var hi = require('../lib/hidash');
 var Kuery = require('..');
 var should = require('should');
 
-
 var collection = [
-  { id: 1, name: 'Andreas', address: { street: 'Bellmansgatan' }, born: new Date('1980-01-01T12:00:00.000Z'), isActive: true },
-  { id: 2, name: 'Sven', address: {}, girlfriends: [{ wife: {} }], born: new Date('1989-01-01T12:00:00.000Z'), isActive: true },
-  { id: 3, name: 'Christian', born: new Date('1990-01-01T12:00:00.000Z'), girlfriends: { wife: {} }, isActive: false },
-  { id: 4, name: 'Emil', girlfriends: [{ name: 'fanny', hotness: 10 }, { name: 'eve', hotness: 1000 }], born: new Date('1982-01-01T12:00:00.000Z') },
-  { id: 5, name: 'PG', girlfriends: [{ name: 'Hanna', hotness: 200 }], born: new Date('1989-01-01T12:00:00.000Z') }
+  {
+    id: 1,
+    name: 'Andreas',
+    address: { street: 'Bellmansgatan' },
+    born: new Date('1980-01-01T12:00:00.000Z'),
+    isActive: true
+  },
+  {
+    id: 2,
+    name: 'Sven',
+    address: {},
+    girlfriends: [{ wife: {} }],
+    born: new Date('1989-01-01T12:00:00.000Z'),
+    isActive: true
+  },
+  {
+    id: 3,
+    name: 'Christian',
+    born: new Date('1990-01-01T12:00:00.000Z'),
+    girlfriends: { wife: {} },
+    isActive: false,
+    parts: [
+      {
+        name: 'part1',
+        parts: []
+      },
+      {
+        name: 'part2',
+        parts: [
+          {
+            name: 'part2.sub1'
+          }
+        ]
+      },
+      {
+        name: 'part3',
+        parts: ''
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Emil',
+    girlfriends: [
+      { name: 'fanny', hotness: 10 },
+      {
+        name: 'eve',
+        hotness: 1000,
+        boyfriends: [
+          { id: 4, name: 'Emil', girlfriends: [{ name: 'fanny', hotness: 10 }, { name: 'eve' }] },
+          { id: 2, name: 'Sven' }
+        ]
+      }
+    ],
+    parts: [
+      {
+        name: 'part1',
+        parts: []
+      },
+      {
+        name: 'part2',
+        parts: [
+          {
+            name: 'part2.sub1'
+          },
+          {
+            name: 'part2.sub2'
+          }
+        ]
+      },
+      {
+        name: 'part3'
+      }
+    ],
+    bikes: [
+      {
+        bike: {
+          brand: 'trek',
+          wheels: [
+            { position: 'front', type: 'carbon' },
+            { position: 'back', type: 'aluminum' }
+          ]
+        }
+      },
+      {
+        bike: {
+          brand: 'unicycle',
+          wheels: [{ position: 'front', type: 'aluminum' }]
+        }
+      }
+    ],
+    currentBike: [{ brand: 'trek', wheels: ['front', 'back'] }],
+    born: new Date('1982-01-01T12:00:00.000Z')
+  },
+  {
+    id: 5,
+    name: 'PG',
+    girlfriends: [{ name: 'Hanna', hotness: 200 }],
+    born: new Date('1989-01-01T12:00:00.000Z'),
+    parts: [
+      {
+        name: 'part1',
+        parts: []
+      },
+      {
+        name: 'part2',
+        parts: [
+          {
+            name: 'part2.sub1'
+          }
+        ]
+      },
+      {
+        name: 'part3',
+        parts: 'TODO'
+      },
+      {
+        name: 'part3',
+        parts: {}
+      }
+    ]
+  }
 ];
 
-var collectionWithNull = [
-  { id: 6, name: 'KE', girlfriends: null }
-];
+var collectionWithNull = [{ id: 6, name: 'KE', girlfriends: null }];
 
 describe('Kuery', function () {
   it('should return 0 for empty collection', function () {
@@ -132,19 +247,13 @@ describe('Kuery', function () {
   });
   it('should return correct elements for $or query', function () {
     var q = new Kuery({
-      $or: [
-        { name: /andr.*/i }
-        , { name: /emil.*/i }
-      ]
+      $or: [{ name: /andr.*/i }, { name: /emil.*/i }]
     });
     q.find(collection).length.should.equal(2);
   });
   it('should return correct elements for $or query when both sides return same element', function () {
     var q = new Kuery({
-      $or: [
-        { name: /andr.*/i },
-        { name: /andr.*/i }
-      ]
+      $or: [{ name: /andr.*/i }, { name: /andr.*/i }]
     });
     q.find(collection).length.should.equal(1);
   });
@@ -189,6 +298,79 @@ describe('Kuery', function () {
     var q = new Kuery({ girlfriends: { $elemMatch: { hotness: 10 } } });
     q.find(collection).length.should.equal(1);
   });
+  it('should not return element when using $elemMatch on an object property', function () {
+    var q = new Kuery({
+      'bikes.bike': { $elemMatch: { brand: 'trek' } }
+    });
+    q.find(collection).length.should.equal(0);
+  });
+  it('should return correct element when using $elemMatch on a nested array property', function () {
+    var q = new Kuery({
+      'bikes.bike.wheels': { $elemMatch: { position: 'front' } }
+    });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct element when using elemMatch on nested optional array property', function () {
+    var q = new Kuery({
+      id: 4,
+      'parts.parts': {
+        $elemMatch: {
+          name: { $eq: 'part2.sub1' }
+        }
+      }
+    });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct element when using $elemMatch on nested array with differing property types', function () {
+    var q = new Kuery({
+      id: 5,
+      'parts.parts': {
+        $elemMatch: {
+          name: { $eq: 'part2.sub1' }
+        }
+      }
+    });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct element when using $elemMatch on nested array where one type is empty string', function () {
+    var q = new Kuery({
+      id: 3,
+      'parts.parts': {
+        $elemMatch: {
+          name: { $eq: 'part2.sub1' }
+        }
+      }
+    });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct element when using $elemMatch with multiple conditions on a nested array property', function () {
+    var q = new Kuery({
+      'bikes.bike.wheels': {
+        $elemMatch: {
+          position: 'front',
+          type: 'carbon'
+        }
+      }
+    });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should not return any element when elemMatch does not match on the same item in the array', function () {
+    var q = new Kuery({
+      'bikes.bike.wheels': {
+        $elemMatch: {
+          position: 'back',
+          type: 'carbon'
+        }
+      }
+    });
+    q.find(collection).length.should.equal(0);
+  });
+  it('should not return any element when $elemMatch is not matching on nested array property', function () {
+    var q = new Kuery({
+      'bikes.bike.wheels': { $elemMatch: { position: 'middle' } }
+    });
+    q.find(collection).length.should.equal(0);
+  });
   it('should return correct element for multipart elemMatch query', function () {
     var q = new Kuery({ girlfriends: { $elemMatch: { hotness: 10, name: 'fanny' } } });
     q.find(collection).length.should.equal(1);
@@ -202,6 +384,18 @@ describe('Kuery', function () {
   it('should return no element for multipart elemMatch query matching different array elements', function () {
     var q = new Kuery({ girlfriends: { $elemMatch: { hotness: 10, name: 'eve' } } });
     q.find(collection).length.should.equal(0);
+  });
+  it('should return correct elements for double nested array elemMatch query', function () {
+    var q = new Kuery({ 'girlfriends.boyfriends': { $elemMatch: { id: 2, name: 'Sven' } } });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct elements for triple nested array elemMatch query', function () {
+    var q = new Kuery({ 'girlfriends.boyfriends.girlfriends': { $elemMatch: { hotness: 10, name: 'fanny' } } });
+    q.find(collection).length.should.equal(1);
+  });
+  it('should return correct elements for negated triple nested array elemMatch query', function () {
+    var q = new Kuery({ 'girlfriends.boyfriends.girlfriends': { $not: { $elemMatch: { hotness: 10, name: 'fanny' } } } });
+    q.find(collection).length.should.equal(4);
   });
   it('should return correct elements for property with path $regexp with arrays', function () {
     var q = new Kuery({ 'girlfriends.name': { $regex: 'ev.*', $options: 'i' } });
@@ -219,8 +413,8 @@ describe('Kuery', function () {
     var q = new Kuery({ 'girlfriends.wife': { $exists: true } });
     q.find(collection).length.should.equal(2);
   });
-  it("should return elements where given element does not exists deeply", function () {
-    var q = new Kuery({ "address.zipcode": { $exists: false } });
+  it('should return elements where given element does not exists deeply', function () {
+    var q = new Kuery({ 'address.zipcode': { $exists: false } });
     q.find(collection).length.should.equal(5);
   });
   it('should return elements when query for boolean', function () {
@@ -239,5 +433,4 @@ describe('Kuery', function () {
     });
     q.find(collection).length.should.equal(1);
   });
-
 });
