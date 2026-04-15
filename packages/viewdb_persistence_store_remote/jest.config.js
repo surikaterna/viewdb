a064076cee4a6f23dfd6594f0ffe1371cd0125f0
sprawl <sprawl@onlajn.nu>
2026-04-15 12:03:40 +0200

    refactor(deps): downgrade uuid to v11 for CJS compatibility
    uuid v12+ dropped CJS support, breaking consumers on Node < 22.12.
    
    v11 is the latest version with dual CJS/ESM packaging.
    
    Remove jest.config.js workaround that was only needed for pure ESM.

--- packages/viewdb_persistence_store_remote/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  transformIgnorePatterns: ['node_modules[/\\\\](?!uuid)'],
-  transform: {
-    '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }]
-  }
-};
