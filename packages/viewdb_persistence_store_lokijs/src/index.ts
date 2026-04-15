bb957b487acca1d78f81df9329a3f5e4a0da9e1e
sprawl <sprawl@onlajn.nu>
2026-04-15 15:34:15 +0200

    fix(exports): use export = to prevent __esModule flag in CJS output
    The TypeScript conversion of remote and lokijs stores used 'export { ... }' which compiles to Object.defineProperty(exports, '__esModule', { value: true }) in the CJS output. The original pre-TS JavaScript used plain module.exports assignments without this flag. This __esModule marker can break consumers relying on ESM default import interop. Switching to 'export = { ... }' produces module.exports = { ... } which matches the original CJS behavior.

--- packages/viewdb_persistence_store_lokijs/src/index.ts
+++ packages/viewdb_persistence_store_lokijs/src/index.ts
@@ -3,4 +3,4 @@ import Collection = require('./collection');
 import LokiPartitioningAdapter = require('./adapter/LokiPartitioningAdapter');
 import LokiCordovaFSAdapter = require('./cordova/LokiCordovaFSAdapter');
 
-export { Store, Collection, LokiPartitioningAdapter, LokiCordovaFSAdapter };
+export = { Store, Collection, LokiPartitioningAdapter, LokiCordovaFSAdapter };
